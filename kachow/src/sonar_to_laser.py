6185ea05479a0a93887c99d68fc5b34565a6cc1c
chamaguilar <86603545+chamaguilar@users.noreply.github.com>
2021-12-10 11:56:22 +0800

    Add files via upload

---
+++ kachow/src/sonar_to_laser.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+import rospy
+import tf
+import math
+import numpy
+from std_msgs.msg import Float32
+from sensor_msgs.msg import LaserScan, Range
+
+rospy.init_node('sonar_to_laser')
+
+# buffer
+b1 = 0.0
+b2 = 0.0
+b3 = 0.0
+
+pub_laserscan = rospy.Publisher('/scan', LaserScan, queue_size=10)
+pub_range = rospy.Publisher('/range', Range, queue_size=10)
+
+Scan_msg = LaserScan()
+Scan_msg.header.frame_id = "sonar_link"
+Scan_msg.angle_min = math.pi/4 - 0.05
+Scan_msg.angle_max = math.pi/4 + 0.05
+Scan_msg.angle_increment = 0.1
+Scan_msg.time_increment = 0.0
+Scan_msg.range_min = 0.05
+Scan_msg.range_max = 0.5
+
+Range_msg = Range()
+Range_msg.header.frame_id = "sonar_link"
+Range_msg.radiation_type = 0
+Range_msg.min_range = 0.05
+Range_msg.max_range = 0.60
+Range_msg.field_of_view = 0.17   # 10deg
+
+def callback(msg):
+
+	# buffer for rejecting false range data
+	global b1, b2, b3
+	b1 = b2
+	b2 = b3
+	b3 = msg.z / 100
+
+	# wait to have more non null ranges before publishing
+	if b1 > 0.0 and b2 > 0.0 and b3 > 0.0:
+
+		Scan_msg.ranges = [msg.z / 100]
+		Range_msg.range = msg.z / 100
+		Scan_msg.header.stamp = rospy.Time.now()
+		Range_msg.header.stamp = rospy.Time.now()
+		pub_laserscan.publish(Scan_msg)
+		pub_range.publish(Range_msg)
+
+	# publish 0.0 (could be false measuremet)
+	else:
+
+		Scan_msg.ranges = [0.0]
+		Range_msg.range = 0.0
+		Scan_msg.header.stamp = rospy.Time.now()
+		Range_msg.header.stamp = rospy.Time.now()
+		pub_laserscan.publish(Scan_msg)
+		pub_range.publish(Range_msg)
+
+
+sub = rospy.Subscriber('sonar', Float32, callback)
+
+rospy.spin()
---
+++ rosserial.ino
@@ -0,0 +1,335 @@
+#include <ros.h>
+#include <ros/time.h>
+#include <std_msgs/Int16.h>
+#include <geometry_msgs/Twist.h>
+#include <sensor_msgs/LaserScan.h>
+#include <AFMotor.h>
+#include <Ultrasonic.h>
+
+ros::NodeHandle nh;
+
+#define ENC_IN_LEFT_A 18
+#define ENC_IN_RIGHT_A 19
+#define ENC_IN_LEFT_B 22
+#define ENC_IN_RIGHT_B 23
+
+boolean Direction_left = true;
+boolean Direction_right = true;
+const int encoder_minimum = -32768;
+const int encoder_maximum = 32767;
+ 
+std_msgs::Int16 right_wheel_tick_count;
+ros::Publisher rightPub("right_ticks", &right_wheel_tick_count);
+std_msgs::Int16 left_wheel_tick_count;
+ros::Publisher leftPub("left_ticks", &left_wheel_tick_count);
+ 
+const int interval = 30;
+long previousMillis = 0;
+long currentMillis = 0;
+
+Ultrasonic leftSonar(24, 25, 100000UL);
+Ultrasonic centerSonar(26, 27, 100000UL);
+Ultrasonic rightSonar(28, 29, 100000UL);
+
+float angleMin;
+float angleMax;
+float angleIncrement;
+float timeIncrement;
+float rangeMin;
+float rangeMax;
+float valRanges[3];
+
+sensor_msgs::LaserScan scan;
+ros::Publisher sonarPub("sonar", &scan);
+
+AF_DCMotor tlMotor(1); // top left, M1
+AF_DCMotor trMotor(4); // top right, M4
+AF_DCMotor blMotor(2); // bottom left, M2
+AF_DCMotor brMotor(3); // bottom right, M3
+
+const int PWM_INCREMENT = 1;
+const int TICKS_PER_REVOLUTION = 3000;
+const double WHEEL_RADIUS = 0.0575;
+const double WHEEL_BASE = 0.285;
+const double TICKS_PER_METER = 8000;
+
+const int K_P = 231;
+const int b = 62;
+
+const int DRIFT_MULTIPLIER = 120;
+const int PWM_TURN = 240;
+const int PWM_MIN = 100;
+const int PWM_MAX = 240;
+
+double velLeftWheel = 0;
+double velRightWheel = 0;
+double pwmLeftReq = 0;
+double pwmRightReq = 0;
+
+double lastCmdVelReceived = 0;
+
+void right_wheel_tick() {
+  int val = digitalRead(ENC_IN_RIGHT_B); 
+  if (val == LOW) {
+    Direction_right = true; // Reverse
+  }
+  else {
+    Direction_right = false; // Forward
+  }
+  if (Direction_right) {  
+    if (right_wheel_tick_count.data == encoder_maximum) {
+      right_wheel_tick_count.data = encoder_minimum;
+    }
+    else {
+      right_wheel_tick_count.data++;  
+    }    
+  }
+  else {
+    if (right_wheel_tick_count.data == encoder_minimum) {
+      right_wheel_tick_count.data = encoder_maximum;
+    }
+    else {
+      right_wheel_tick_count.data--;  
+    }
+  }
+}
+
+void left_wheel_tick() {
+  int val = digitalRead(ENC_IN_LEFT_B);
+  if (val == LOW) {
+    Direction_left = false; // Reverse
+  }
+  else {
+    Direction_left = true; // Forward
+  }
+  if (Direction_left) {
+    if (left_wheel_tick_count.data == encoder_maximum) {
+      left_wheel_tick_count.data = encoder_minimum;
+    }
+    else {
+      left_wheel_tick_count.data++;  
+    }  
+  }
+  else {
+    if (left_wheel_tick_count.data == encoder_minimum) {
+      left_wheel_tick_count.data = encoder_maximum;
+    }
+    else {
+      left_wheel_tick_count.data--;  
+    }   
+  }
+}
+ 
+void calc_vel_left_wheel(){
+  static double prevTime = 0;
+  static int prevLeftCount = 0;
+  int numOfTicks = (65535 + left_wheel_tick_count.data - prevLeftCount) % 65535;
+  if (numOfTicks > 10000) {
+        numOfTicks = 0 - (65535 - numOfTicks);
+  }
+  velLeftWheel = numOfTicks/TICKS_PER_METER/((millis()/1000)-prevTime);
+  prevLeftCount = left_wheel_tick_count.data;
+  prevTime = (millis()/1000);
+}
+
+void calc_vel_right_wheel(){
+  static double prevTime = 0;
+  static int prevRightCount = 0;
+  int numOfTicks = (65535 + right_wheel_tick_count.data - prevRightCount) % 65535;
+  if (numOfTicks > 10000) {
+        numOfTicks = 0 - (65535 - numOfTicks);
+  }
+  velRightWheel = numOfTicks/TICKS_PER_METER/((millis()/1000)-prevTime);
+  prevRightCount = right_wheel_tick_count.data;
+  prevTime = (millis()/1000); 
+}
+
+void calc_pwm_values(const geometry_msgs::Twist& cmdVel) {
+  lastCmdVelReceived = (millis()/1000);
+  pwmLeftReq = K_P * cmdVel.linear.x + b;
+  pwmRightReq = K_P * cmdVel.linear.x + b;
+ 
+  // Check if we need to turn 
+  if (cmdVel.angular.z != 0.0) {
+    // Turn left
+    if (cmdVel.angular.z > 0.0) {
+      pwmLeftReq = -PWM_TURN;
+      pwmRightReq = PWM_TURN;
+    }
+    // Turn right    
+    else {
+      pwmLeftReq = PWM_TURN;
+      pwmRightReq = -PWM_TURN;
+    }
+  }
+  // Go straight
+  else {
+    static double prevDiff = 0;
+    static double prevPrevDiff = 0;
+    double currDifference = velLeftWheel - velRightWheel; 
+    double avgDifference = (prevDiff+prevPrevDiff+currDifference)/3;
+    prevPrevDiff = prevDiff;
+    prevDiff = currDifference;
+
+    pwmLeftReq -= (int)(avgDifference * DRIFT_MULTIPLIER);
+    pwmRightReq += (int)(avgDifference * DRIFT_MULTIPLIER);
+  }
+  
+  // Handle low PWM values
+  if (abs(pwmLeftReq) < PWM_MIN) {
+    pwmLeftReq = 0;
+  }
+  if (abs(pwmRightReq) < PWM_MIN) {
+    pwmRightReq = 0;  
+  }  
+}
+
+void set_pwm_values() {
+  static int pwmLeftOut = 0;
+  static int pwmRightOut = 0;
+  static bool stopped = false;
+  if ((pwmLeftReq * velLeftWheel < 0 && pwmLeftOut != 0) ||
+      (pwmRightReq * velRightWheel < 0 && pwmRightOut != 0)) {
+    pwmLeftReq = 0;
+    pwmRightReq = 0;
+  }
+ 
+  // Set the direction of the motors
+  if (pwmLeftReq > 0) { // Left wheel forward
+    tlMotor.run(FORWARD);
+    blMotor.run(FORWARD);
+  }
+  else if (pwmLeftReq < 0) { // Left wheel reverse
+    tlMotor.run(BACKWARD);
+    blMotor.run(BACKWARD);
+  }
+  else if (pwmLeftReq == 0 && pwmLeftOut == 0 ) { // Left wheel stop
+    tlMotor.run(RELEASE);
+    blMotor.run(RELEASE);
+  }
+  else { // Left wheel stop
+    tlMotor.run(RELEASE);
+    blMotor.run(RELEASE); 
+  }
+ 
+  if (pwmRightReq > 0) { // Right wheel forward
+    trMotor.run(BACKWARD);
+    brMotor.run(BACKWARD);
+  }
+  else if(pwmRightReq < 0) { // Right wheel reverse
+    trMotor.run(FORWARD);
+    brMotor.run(FORWARD);
+  }
+  else if (pwmRightReq == 0 && pwmRightOut == 0) { // Right wheel stop
+    trMotor.run(RELEASE);
+    brMotor.run(RELEASE);
+  }
+  else { // Right wheel stop
+    trMotor.run(RELEASE);
+    brMotor.run(RELEASE);
+  }
+ 
+  // Increase the required PWM if the robot is not moving
+  if (pwmLeftReq != 0 && velLeftWheel == 0) {
+    pwmLeftReq *= 1.5;
+  }
+  if (pwmRightReq != 0 && velRightWheel == 0) {
+    pwmRightReq *= 1.5;
+  }
+ 
+  // Calculate the output PWM value by making slow changes to the current value
+  if (abs(pwmLeftReq) > pwmLeftOut) {
+    pwmLeftOut += PWM_INCREMENT;
+  }
+  else if (abs(pwmLeftReq) < pwmLeftOut) {
+    pwmLeftOut -= PWM_INCREMENT;
+  }
+  else{}
+   
+  if (abs(pwmRightReq) > pwmRightOut) {
+    pwmRightOut += PWM_INCREMENT;
+  }
+  else if(abs(pwmRightReq) < pwmRightOut) {
+    pwmRightOut -= PWM_INCREMENT;
+  }
+  else{}
+ 
+  // Conditional operator to limit PWM output at the maximum 
+  pwmLeftOut = (pwmLeftOut > PWM_MAX) ? PWM_MAX : pwmLeftOut;
+  pwmRightOut = (pwmRightOut > PWM_MAX) ? PWM_MAX : pwmRightOut;
+ 
+  // PWM output cannot be less than 0
+  pwmLeftOut = (pwmLeftOut < 0) ? 0 : pwmLeftOut;
+  pwmRightOut = (pwmRightOut < 0) ? 0 : pwmRightOut;
+
+  tlMotor.setSpeed(pwmLeftOut);
+  blMotor.setSpeed(pwmLeftOut);
+  trMotor.setSpeed(pwmRightOut);
+  brMotor.setSpeed(pwmRightOut);
+}
+ 
+ros::Subscriber<geometry_msgs::Twist> subCmdVel("cmd_vel", &calc_pwm_values );
+ 
+void setup() {
+  pinMode(ENC_IN_LEFT_A , INPUT_PULLUP);
+  pinMode(ENC_IN_LEFT_B , INPUT);
+  pinMode(ENC_IN_RIGHT_A , INPUT_PULLUP);
+  pinMode(ENC_IN_RIGHT_B , INPUT);
+  attachInterrupt(digitalPinToInterrupt(ENC_IN_LEFT_A), left_wheel_tick, RISING);
+  attachInterrupt(digitalPinToInterrupt(ENC_IN_RIGHT_A), right_wheel_tick, RISING);
+
+  tlMotor.setSpeed(0);
+  trMotor.setSpeed(0);
+  blMotor.setSpeed(0);
+  brMotor.setSpeed(0);
+
+  angleMin = -1.57;
+  angleMax = 1.57;
+  angleIncrement = 3.14 / 3;
+  timeIncrement = 1 / 6;
+  rangeMin = 0.0;
+  rangeMax = 10.0;
+  scan.ranges_length = 3;
+  for (int i=0; i<=2; i++) {
+    valRanges[i] = i;
+  }
+
+  nh.getHardware()->setBaud(115200);
+  nh.initNode();
+  nh.advertise(rightPub);
+  nh.advertise(leftPub);
+  nh.advertise(sonarPub);
+  nh.subscribe(subCmdVel);
+}
+ 
+void loop() {
+  nh.spinOnce();
+
+  currentMillis = millis();
+  if (currentMillis - previousMillis > interval) {
+    previousMillis = currentMillis;
+
+    valRanges[0] = rightSonar.read() / 100.0;
+    valRanges[1] = centerSonar.read() / 100.0;
+    valRanges[2] = leftSonar.read() / 100.0;
+
+    scan.header.stamp = nh.now();
+    scan.header.frame_id = "ultrasound";
+    scan.angle_min = angleMin;
+    scan.angle_max = angleMax;
+    scan.angle_increment = angleIncrement;
+    scan.time_increment = timeIncrement;
+    scan.range_min = rangeMin;
+    scan.range_max = rangeMax;
+    scan.ranges = valRanges;
+    sonarPub.publish( &scan );
+    
+    leftPub.publish( &left_wheel_tick_count );
+    rightPub.publish( &right_wheel_tick_count );
+
+    calc_vel_right_wheel();
+    calc_vel_left_wheel();
+  }
+  
+  set_pwm_values();
+}
